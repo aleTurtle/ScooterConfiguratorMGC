12808d8ca1a0d9c02298414e5b5164a0beb82dfe
turtle <alessio.tartufoli@studenti.unicam.it>
2024-12-31 12:13:44 +0100

    conflitti con il reasoner

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,7 @@ module it.didattica.cs.unicam.progettoloretimgc {
     requires org.apache.jena.arq;
     requires org.apache.jena.core;
     requires java.net.http;
-    requires openllet.jena;
+    //requires openllet.jena;
 
 
     opens it.didattica.cs.unicam.progettoloretimgc to javafx.fxml;
